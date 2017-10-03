3368abda44fca64167f3b385955c84cf4eb1f500
Nkmol <sandermol95@hotmail.com>
2017-10-03 19:43:32 +0200

    Basic interperter factory

---
+++ DPA_Musicsheets/New/Compiler/Compiler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DPA_Musicsheets.Models;
+
+namespace DPA_Musicsheets.New.Compiler
+{
+    public class Compiler
+    {
+        private readonly LinkedList<LilypondToken> _tokens;
+        private readonly CompilerFactory _factory;
+
+        public Compiler(LinkedList<LilypondToken> tokens)
+        {
+            _tokens = tokens;
+            _factory = new CompilerFactory();
+        }
+
+        public void Run()
+        {
+            foreach (var lilypondToken in _tokens)
+            {
+                var statement = _factory.Create(lilypondToken.TokenKind.ToString());
+                statement?.Compile(lilypondToken);
+            }
+        }
+    }
+}
---
+++ DPA_Musicsheets/New/Compiler/CompilerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datatypes;
+using DPA_Musicsheets.Models;
+using DPA_Musicsheets.New.Compiler.Statements;
+using Helpers.Datatypes;
+
+namespace DPA_Musicsheets.New.Compiler
+{
+    public class CompilerFactory : Singleton<CompilerFactory>, IFactory<ICompilerStatement>
+    {
+        private readonly IFactory<ICompilerStatement> _factory = new Factory<ICompilerStatement>();
+
+        public CompilerFactory()
+        {
+            AddType(LilypondTokenKind.Note.ToString(), typeof(CompilerNote));
+        }
+
+        public void AddType(string typenaming, Type type)
+        {
+            _factory.AddType(typenaming, type);
+        }
+
+        public ICompilerStatement Create(string type)
+        {
+            return _factory.Create(type);
+        }
+    }
+}
